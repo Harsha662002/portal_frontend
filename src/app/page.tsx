6bbe39bfe2f9ca85697ef604f14e57f901f4d830
harsha7innerscore <engg.intern12@coschool.ai>
2025-01-19 19:46:33 +0530

    Added context, login basic flow, home page, updated theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { useAppContext } from "@/context/AppContext";
+import HomePage from "./home/page";
+import LoginPage from "./login/page";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
-    </main>
-  );
+  const { isLogin } = useAppContext();
+  return <main>{isLogin ? <HomePage /> : <LoginPage />}</main>;
 }
